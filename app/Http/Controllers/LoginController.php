90507617ec97473bb81f5c781cd2ae80995b54ba
Ays1234 <yogimaulana100@gmail.com>
2023-02-09 16:47:52 +0700

    project data pertama

---
+++ app/Http/Controllers/LoginController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+
+class LoginController extends Controller
+{
+    //
+    public function index()
+    {
+        return view('login.index', [
+            'title' => 'Login',
+        ]);
+    }
+
+    public function actionmasuk(Request $request)
+    {
+        $periksa = $request->validate(
+            [
+                'email' => 'required|email:dns',
+                'password' => 'required'
+            ]
+        );
+        if (Auth::attempt($periksa)) {
+            $request->session()->regenerate();
+            return redirect()->intended('/dashboard');
+        }
+        dd('berhasil login');
+    }
+}
